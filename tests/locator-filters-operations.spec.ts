c4db14f6265e86418e76493f68badf5db7923545
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-10-20 20:57:59 +0200

    add tests for locating buttons in table using chained and filter locators

--- tests/locator-filters-operations.spec.ts
+++ tests/locator-filters-operations.spec.ts
@@ -37,5 +37,39 @@ test.describe('Locator filters', () => {
             await expect(resultsLocator).toHaveText(expectedMessage);
         });
     });
+    test.describe("Buttons in table - different approaches", () => {
+        test('Single button click (chained getBy)', async ({ page }) => {
+            // Arrange
+            const elementRole = 'button';
+            const resultsTestId = 'dti-results';
+            const parentText = 'Row 2';
+            const parentRole = 'row';
+            const expectedMessage = 'You clicked the button! (row 2)';
+
+            const resultsLocator = page.getByTestId(resultsTestId);
+            const buttonLocator = page.getByRole(parentRole, { name: parentText }).getByRole(elementRole);
+            // Act
+            await buttonLocator.click();
+            // Assert
+            await expect(resultsLocator).toHaveText(expectedMessage);
+        });
+    });
+    test.describe("Buttons in table - different approaches", () => {
+        test('Single button click (using filter)', async ({ page }) => {
+            // Arrange
+            const elementRole = 'button';
+            const resultsTestId = 'dti-results';
+            const parentText = 'Row 2';
+            const parentRole = 'row';
+            const expectedMessage = 'You clicked the button! (row 2)';
+
+            const resultsLocator = page.getByTestId(resultsTestId);
+            const buttonLocator = page.getByRole(parentRole).filter({ has: page.getByText("Row 2") }).getByRole(elementRole);
+            // Act
+            await buttonLocator.click();
+            // Assert
+            await expect(resultsLocator).toHaveText(expectedMessage);
+        });
+    });
 
 })
